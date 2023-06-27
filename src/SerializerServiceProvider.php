9e16862626a606ba7f6e6306461bd76934d95e4a
pjordaan <pjordaan@users.noreply.github.com>
2023-06-27 14:30:36 +0000

    Update file(s) "packages/serializer/." from "apie-lib/apie-lib-monorepo"

---
+++ src/SerializerServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+namespace Apie\Serializer;
+
+use Apie\ServiceProviderGenerator\UseGeneratedMethods;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * This file is generated with apie/service-provider-generator from file: serializer.yaml
+ * @codecoverageIgnore
+ * @phpstan-ignore
+ */
+class SerializerServiceProvider extends ServiceProvider
+{
+    use UseGeneratedMethods;
+
+    public function register()
+    {
+        $this->app->singleton(
+            \Apie\Serializer\Serializer::class,
+            function ($app) {
+                return \Apie\Serializer\Serializer::create(
+                
+                );
+                
+            }
+        );
+        $this->app->singleton(
+            \Apie\Serializer\EncoderHashmap::class,
+            function ($app) {
+                return \Apie\Serializer\EncoderHashmap::create(
+                
+                );
+                
+            }
+        );
+        $this->app->singleton(
+            \Apie\Serializer\DecoderHashmap::class,
+            function ($app) {
+                return \Apie\Serializer\DecoderHashmap::create(
+                
+                );
+                
+            }
+        );
+        
+    }
+}
