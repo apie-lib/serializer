1b4ed72fcf2dea8fbf669dbdcbae2c85354accc4
pjordaan <pjordaan@users.noreply.github.com>
2023-07-09 14:59:10 +0000

    Update file(s) "packages/serializer/." from "apie-lib/apie-lib-monorepo"

--- src/SerializerServiceProvider.php
+++ src/SerializerServiceProvider.php
@@ -7,7 +7,6 @@ use Illuminate\Support\ServiceProvider;
 /**
  * This file is generated with apie/service-provider-generator from file: serializer.yaml
  * @codecoverageIgnore
- * @phpstan-ignore
  */
 class SerializerServiceProvider extends ServiceProvider
 {
