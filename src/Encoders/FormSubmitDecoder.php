a89c423d453fd85d7f1afbf3f83a2d187d1016ac
pjordaan <pjordaan@users.noreply.github.com>
2024-01-29 05:13:13 +0000

    Update file(s) "packages/serializer/." from "apie-lib/apie-lib-monorepo"

--- src/Encoders/FormSubmitDecoder.php
+++ src/Encoders/FormSubmitDecoder.php
@@ -2,6 +2,8 @@
 namespace Apie\Serializer\Encoders;
 
 use Apie\Serializer\Interfaces\DecoderInterface;
+use stdClass;
+use UnexpectedValueException;
 
 class FormSubmitDecoder implements DecoderInterface
 {
@@ -11,7 +13,36 @@ class FormSubmitDecoder implements DecoderInterface
         $data = $formContents['form'] ?? [];
         $data['_csrf'] = $formContents['_csrf'] ?? 'no csrf';
         // TODO internal data to add missing fields that are not compatible
+        foreach ($formContents['_apie']['typehint'] ?? [] as $key => $typehintData) {
+            $this->fillIn($data, $key, $typehintData);
+        }
 
         return $data;
+    }
+
+    private function fillIn(array& $data, string $key, string|int|float|bool|array|null $typehintData): void
+    {
+        if (!isset($data[$key])) {
+            if (is_array($typehintData)) {
+                $data[$key] = [];
+            } else {
+                $data[$key] = match($typehintData) {
+                    'string' => '',
+                    'int' => 0,
+                    'float' => 0.0,
+                    'bool' => false,
+                    'null' => null,
+                    'array' => [],
+                    'object' => [],
+                    default => throw new UnexpectedValueException('Expected string|int|float|bool|null|array|object, got "' . $typehintData . '"')
+                };
+                return;
+            }
+        }
+        if (is_array($typehintData)) {
+            foreach ($typehintData as $subKey => $typehintSubdata) {
+                $this->fillIn($data[$key], $subKey, $typehintSubdata);
+            }
+        }
     }
 }
