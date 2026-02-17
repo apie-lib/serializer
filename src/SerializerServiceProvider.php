8c4670524c3467729876ff026f048f0d5681bf65
pjordaan <pjordaan@users.noreply.github.com>
2026-02-17 06:13:21 +0000

    Update file(s) "packages/serializer/." from "apie-lib/apie-lib-monorepo"

--- src/SerializerServiceProvider.php
+++ src/SerializerServiceProvider.php
@@ -14,7 +14,7 @@ class SerializerServiceProvider extends ServiceProvider
 
     public function register()
     {
-        $this->app->singleton(
+        $this->registerSingleton(
             \Apie\Serializer\Serializer::class,
             function ($app) {
                 return \Apie\Serializer\Serializer::create(
@@ -34,7 +34,7 @@ class SerializerServiceProvider extends ServiceProvider
             )
         );
         $this->app->tag([\Apie\Serializer\Serializer::class], 'apie.context');
-        $this->app->singleton(
+        $this->registerSingleton(
             \Apie\Serializer\EncoderHashmap::class,
             function ($app) {
                 return \Apie\Serializer\EncoderHashmap::create(
@@ -43,7 +43,7 @@ class SerializerServiceProvider extends ServiceProvider
                 
             }
         );
-        $this->app->singleton(
+        $this->registerSingleton(
             \Apie\Serializer\DecoderHashmap::class,
             function ($app) {
                 return \Apie\Serializer\DecoderHashmap::create(
