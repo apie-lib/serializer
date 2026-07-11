70d270a4c932b46ac161f7f9bf09263442682a4b
pjordaan <pjordaan@users.noreply.github.com>
2026-07-11 07:52:36 +0000

    Update file(s) "packages/serializer/." from "apie-lib/apie-lib-monorepo"

--- src/Exceptions/NotAcceptedException.php
+++ src/Exceptions/NotAcceptedException.php
@@ -6,9 +6,9 @@ use Apie\Core\Exceptions\HttpStatusCodeException;
 
 class NotAcceptedException extends ApieException implements HttpStatusCodeException
 {
-    public function __construct(string $acceptHeader)
+    public function __construct(string $acceptHeader, ?\Throwable $previous = null)
     {
-        parent::__construct(sprintf('Accept header: "%s" not accepted!', $acceptHeader));
+        parent::__construct(sprintf('Accept header: "%s" not accepted!', $acceptHeader), 0, $previous);
     }
 
     public function getStatusCode(): int
